da06eee843e768e33c3b4bcf2f0c852fccceeb1d
fanxiaoping <18324485857>
2018-08-04 18:09:46 +0800

    init

---
+++ basics/src/main/java/com/xingkong/spingboot/Test/Valuation.java
@@ -0,0 +1,36 @@
+package com.xingkong.spingboot.Test;
+
+/**
+ * description:赋值
+ * Created by 46926 on 2018/8/4.
+ * 别名现象
+ */
+public class Valuation {
+
+    static class Tank{
+        int level;
+    }
+
+    public static void main(String[] args) {
+        Tank t1 = new Tank();
+        t1.level = 10;
+        Tank t2 = new Tank();
+        t2.level =100;
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//10,100
+        t1 = t2;
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//100,100
+        t1.level = 50;
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//50,50
+
+        t1.level = t2.level;
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//50,50
+        t2.level = 166;
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//166,166
+        f(t1);
+        System.out.println("t1 = "+t1.level+";t2 = "+t2.level);//196,196
+    }
+
+    private static void f(Tank tank){
+        tank.level = 196;
+    }
+}
---
+++ basics/src/test/java/com/xingkong/spingboot/BasicsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xingkong.spingboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
