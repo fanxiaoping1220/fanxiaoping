af75206ce8b953f4eed2b87b9082fdf14b8b5980
fanxiaoping <18324485857>
2018-08-04 20:11:39 +0800

    添加BigDecimal的精确运算+ - * /

---
+++ basics/src/main/java/com/xingkong/spingboot/Test/Arithmetic.java
@@ -0,0 +1,24 @@
+package com.xingkong.spingboot.Test;
+
+import com.xingkong.spingboot.common.AccurateCalculation;
+
+/**
+ * 算术操作符
+ * Created by 46926 on 2018/8/4.
+ */
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        /**
+         * 总长度为1500.999米
+         * 每分钟为10米
+         * 需要多久
+         */
+        double distance = 1505.999;
+        double sum = 10;
+        System.out.println(AccurateCalculation.add(distance,sum,2));//1516.00
+        System.out.println(AccurateCalculation.sub(distance,sum,2));//1496.00
+        System.out.println(AccurateCalculation.mul(distance,sum,2));//15059.99
+        System.out.println(AccurateCalculation.div(distance,sum,2));//150.6
+    }
+}
