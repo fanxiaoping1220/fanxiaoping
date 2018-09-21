b91d0f91579388befc4168b2acd9aba8b760c5c7
xingkong <xing@kong.com>
2018-09-21 12:00:37 +0800

    Thread面试题

---
+++ basics/src/main/java/com/xingkong/spingboot/entity/ThreadDome.java
@@ -0,0 +1,32 @@
+package com.xingkong.spingboot.entity;
+
+/**
+ * @ClassName ThreadDome
+ * @Description
+ * @Author fanxiaoping
+ * @Date 2018/9/21 11:10
+ * @Version 1.0.0
+ **/
+public class ThreadDome implements Runnable {
+
+    private boolean flag = false;
+
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(200);
+        }catch (Exception e){
+
+        }
+        flag = true;
+        System.out.println("flag="+flag);
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+}
