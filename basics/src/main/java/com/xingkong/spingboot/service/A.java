4d04b03399b186e5cb5ce0f1b9a34a78a820591b
fanxp <fxp@example.com>
2019-09-08 16:20:30 +0800

    done

---
+++ basics/src/main/java/com/xingkong/spingboot/service/A.java
@@ -0,0 +1,11 @@
+package com.xingkong.spingboot.service;
+
+/**
+ * @className: A
+ * @description: 测试
+ * @author: 范小平
+ * @date: 2019-09-08 16:15
+ * @version: 1.0.0
+ */
+public interface A {
+}
---
+++ basics/src/main/java/com/xingkong/spingboot/service/impl/AImpl.java
@@ -0,0 +1,16 @@
+package com.xingkong.spingboot.service.impl;
+
+import com.xingkong.spingboot.service.A;
+import org.springframework.stereotype.Service;
+
+/**
+ * @className: AImpl
+ * @description: 测试
+ * @author: 范小平
+ * @date: 2019-09-08 16:16
+ * @version: 1.0.0
+ */
+@Service
+public class AImpl implements A {
+
+}
