9498e5a093e7bb17d9b5c9d1f91bf4113eec7158
fanxp <fxp@example.com>
2019-11-01 11:35:20 +0800

    添加sping boot 的定时任务的配置@Scheduled

---
+++ basics/src/main/java/com/xingkong/spingboot/config/ScheduledConfig.java
@@ -0,0 +1,32 @@
+package com.xingkong.spingboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+/**
+ * @className: ScheduledConfig
+ * @description: 定时任务配置
+ * @author: 范小平
+ * @date: 2019-11-01 10:53
+ * @version: 1.0.0
+ */
+@Configuration
+@EnableAsync
+public class ScheduledConfig {
+
+    /**
+     * 每一个定时任务每次创建一个新的线程
+     * 创建一个定时任务的线程池size为100
+     * @return
+     */
+    @Bean
+    public TaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(100);
+        return taskScheduler;
+    }
+
+}
