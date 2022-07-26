fb9e9fece463264c82125d9b7008ea570a1da9b5
范小平 <you@example.com>
2022-07-26 16:46:07 +0800

    反射

---
+++ basics/src/main/java/com/xingkong/spingboot/reflection/Test.java
@@ -0,0 +1,36 @@
+package com.xingkong.spingboot.reflection;
+
+import lombok.Data;
+
+/**
+ * * @className: Test
+ * * @description: 测试反射
+ * * @author: fan xiaoping
+ * * @date: 2022/7/26 0026 16:38
+ **/
+public class Test {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> c1 = Class.forName("com.xingkong.spingboot.reflection.User");
+        System.out.println(c1);
+        //一个类在内存中只有一个class对象
+        //一个类被加载后，类的整个结果都会封装在class对象中
+        Class<?> c2 = Class.forName("com.xingkong.spingboot.reflection.User");
+        Class<?> c3 = Class.forName("com.xingkong.spingboot.reflection.User");
+        Class<?> c4 = Class.forName("com.xingkong.spingboot.reflection.User");
+        System.out.println(c2.hashCode());
+        System.out.println(c3.hashCode());
+        System.out.println(c4.hashCode());
+    }
+}
+
+@Data
+class User{
+
+    private String name;
+
+    private Integer age;
+
+    private Integer id;
+}
+
