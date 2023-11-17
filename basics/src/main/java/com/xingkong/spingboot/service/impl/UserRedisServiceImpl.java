e54c83ae371763b501f8c0bd6d20a6434f3e160d
范小平 <you@example.com>
2023-11-17 17:30:01 +0800

    redis user

--- basics/src/main/java/com/xingkong/spingboot/service/impl/UserRedisServiceImpl.java
+++ basics/src/main/java/com/xingkong/spingboot/service/impl/UserRedisServiceImpl.java
@@ -6,6 +6,7 @@ import com.xingkong.spingboot.mapper.UserRedisMapper;
 import com.xingkong.spingboot.service.UserRedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +27,7 @@ public class UserRedisServiceImpl implements UserRedisService {
     @Autowired
     private RedisUtil redisUtil;
 
+    @Transactional
     @Override
     public void addUser(UserRedis user) {
         user.setCreateTime(LocalDateTime.now());
