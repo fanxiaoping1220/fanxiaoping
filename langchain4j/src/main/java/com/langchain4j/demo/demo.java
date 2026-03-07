0d74d12c7c884a5b11d159ddcfb2206b3bc137b1
fanxiaoping <fan18324485857@163.com>
2026-03-08 00:04:49 +0800

    fix(demo): 修复变量名拼写错误
    - 将变量名 mode 改为 model
    - 确保变量名与实际对象实例一致

--- langchain4j/src/main/java/com/langchain4j/demo/demo.java
+++ langchain4j/src/main/java/com/langchain4j/demo/demo.java
@@ -7,7 +7,7 @@ public class demo {
     public static void main(String[] args) {
         // 创建模型
         System.out.println(System.getenv("QWEN_API_KEY"));
-        OpenAiChatModel mode = OpenAiChatModel.builder()
+        OpenAiChatModel model = OpenAiChatModel.builder()
                 .baseUrl("https://dashscope.aliyuncs.com/compatible-mode/v1")
                 .apiKey(System.getenv("QWEN_API_KEY"))
                 .modelName("qwen-plus")
@@ -15,7 +15,7 @@ public class demo {
                 .logResponses(true)
                 .build();
         // 调用模型
-        String result = mode.chat("HTTP请求方式有哪些，请一一例举，并逐个说明");
+        String result = model.chat("HTTP请求方式有哪些，请一一例举，并逐个说明");
         System.out.println(result);
     }
 }
