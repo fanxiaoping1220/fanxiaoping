cee888a2bd63ab36e6cbb219b8f9802f453ce89c
fanxiaoping <fan18324485857@163.com>
2026-03-14 23:42:58 +0800

    feat(chat): 添加流式聊天功能
    实现流式返回

--- springAI/src/main/java/com/springai/controller/ChatController.java
+++ springAI/src/main/java/com/springai/controller/ChatController.java
@@ -1,11 +1,14 @@
 package com.springai.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RequestMapping("/chat")
 @RestController
@@ -14,10 +17,31 @@ public class ChatController {
 
     private final DeepSeekChatModel deepSeekChatModel;
 
+    /**
+     * 聊天
+     * @param message
+     * @return
+     */
     @GetMapping
     public String generate(@RequestParam(value = "message", defaultValue = "你是谁") String message){
         System.out.println("message="+message);
         String response = deepSeekChatModel.call(message);
+        System.out.println("response="+response);
+        return response;
+    }
+
+    /**
+     * 流式聊天
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁") String message,
+                                       HttpServletResponse httpServletResponse){
+        //设置响应体编码为UTF-8
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        System.out.println("message="+message);
+        Flux<String> response = deepSeekChatModel.stream(message);
         System.out.println("response="+response);
         return response;
     }
